261cd6130d4da74ee6554f90e2c36139e0d77320
Mike Halma <mike_halma@yahoo.co.uk>
2018-12-09 19:58:21 +0000

    Refactor

--- src/main/kotlin/findBoxes.kt
+++ src/main/kotlin/findBoxes.kt
@@ -1,43 +1,28 @@
 fun charDiffCountIsOne(boxIds: Pair<String, String>): Boolean {
-    var oneDiff = false
-    boxIds.first.toCharArray().forEachIndexed { index, char ->
-        if (boxIds.second.toCharArray()[index] != char) {
-            if (!oneDiff) {
-                oneDiff = true
-            } else {
-                return false
-            }
-        }
-    }
-    return oneDiff
+    return zipChars(boxIds)
+        .filter { it.first != it.second }
+        .count() == 1
 }
 
+private fun zipChars(boxIds: Pair<String, String>) =
+    boxIds.first.toCharArray().zip(boxIds.second.toCharArray())
+
 fun findBoxIdsWithOneDiff(boxIds: List<String>): Pair<String, String>? {
-    var firstBoxIds = boxIds
-    var secondBoxIds = firstBoxIds.drop(1)
-    while (secondBoxIds.size > 0) {
-        for (firstBoxId in firstBoxIds) {
-            for (secondBoxId in secondBoxIds) {
-                val pairOfBoxIds = Pair(firstBoxId, secondBoxId)
-                if (charDiffCountIsOne(pairOfBoxIds)) {
-                    return pairOfBoxIds
-                }
-            }
-        }
-        firstBoxIds = secondBoxIds
-        secondBoxIds = secondBoxIds.drop(1)
-    }
-    return null
+    if (boxIds.size < 2) return null
+
+    val oneDiffPair : Pair<String, String>? =
+        boxIds.drop(1)
+            .map {Pair(boxIds[0], it)}
+            .firstOrNull {charDiffCountIsOne(it)}
+
+    return oneDiffPair?:findBoxIdsWithOneDiff(boxIds.drop(1))
 }
 
 fun extractCommonChars(boxIds: Pair<String, String>): String {
-    val commonChars = mutableListOf<Char>()
-    boxIds.first.toCharArray().forEachIndexed { index, char ->
-        if (char == boxIds.second.toCharArray()[index]) {
-            commonChars.add(char)
-        }
-    }
-    return commonChars.joinToString().replace(", ", "")
+    return zipChars(boxIds)
+        .filter { it.first == it.second}
+        .map { it.first }
+        .joinToString("")
 }
 
 fun findBoxes(boxIds: List<String>) :String {
